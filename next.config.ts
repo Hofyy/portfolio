45411ecdea869249800c7f3efe38daa2943402b8
Hofy <96525457+Hofyy@users.noreply.github.com>
2026-02-24 13:54:14 +0100

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: isProd ? "/repo-name" : "",
+  assetPrefix: isProd ? "/repo-name/" : ""
 };
 
 export default nextConfig;
